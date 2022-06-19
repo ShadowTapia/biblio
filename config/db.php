71769558824c232393742fbc95aeaa65c254fca3
ShadowTapia <chelo.tapia@gmail.com>
2022-06-18 21:14:21 -0400

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=biblio',
     'username' => 'SysBiblio',
-    'password' => 'F5xDwbSL8q9yHKqM',
+    'password' => '123456',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
