b9fcf024efdd01916456c88f455677391a24134f
Marcelo Tapia <chelo.tapia@gmail.com>
2023-07-14 17:13:36 -0400

    Modificación de la vista update user

--- views/users/updateuser.php
+++ views/users/updateuser.php
@@ -25,25 +25,42 @@ $this->params['breadcrumbs'][] = $this->title;
     'enableClientValidation' => true,
 ]);
 ?>
-<div class="form-group col-xs-4">
-    <?= $form->field($model, "UserName")->input("text", ['width:450px;'])->label('Nombre Usuario') ?>
+<div class="row">
+    <div class="form-group col-xs-4">
+        <?= $form->field($model, "UserName")->input("text", ['width:450px;'])->label('Nombre Usuario') ?>
+    </div>
+    <div class="form-group col-xs-4">
+        <?= $form->field($model, "UserLastName")->input("text", ['width:450px;'])->label('Apellido Usuario') ?>
+    </div>
+    <div class="form-group col-xs-4">
+        <?= $form->field($model, "UserMail")->input("text", ['width:450px'])->label('Email Usuario') ?>
+    </div>
 </div>
-<div class="form-group col-xs-4">
-    <?= $form->field($model, "UserLastName")->input("text", ['width:450px;'])->label('Apellido Usuario') ?>
+<div class="row">
+    <div class="form-group col-xs-4">
+        <?=
+        $form->field($model, "idroles")->dropDownList(
+            ArrayHelper::map(Roles::find()->all(), 'idroles', 'nombre')
+        )->label('Roles')
+        ?>
+    </div>
+    <div class="col-xs-offset-0">
+        <?= $form->field($model, "activate")->widget(
+            SwitchInput::class,
+            [
+                'type' => SwitchInput::CHECKBOX, 'pluginOptions' =>
+                [
+                    'handleWidth' => 20,
+                    'onText' => '<i class="glyphicon glyphicon-ok"></i>',
+                    'offText' => '<i class="glyphicon glyphicon-remove"></i>',
+                    'onColor' => 'success',
+                    'offColor' => 'danger',
+                ],
+            ]
+        )->label('Activo') ?>
+    </div>
 </div>
-<div class="form-group col-xs-4">
-    <?= $form->field($model, "UserMail")->input("text", ['width:450px'])->label('Email Usuario') ?>
+<div class="row">
+    <?= Html::submitButton('Modificar', ['class' => 'btn btn-primary']) ?>
 </div>
-<div class="form-group col-xs-4">
-    <?=
-    $form->field($model, "idroles")->dropDownList(
-        ArrayHelper::map(Roles::find()->all(), 'idroles', 'nombre')
-    )->label('Roles')
-    ?>
-</div>
-<div class="form-group">
-    <?= $form->field($model, "activate")->widget(SwitchInput::class, ['type' => SwitchInput::CHECKBOX, 'pluginOptions' => ['size' => 'small', 'onText' => '<i class="glyphicon glyphicon-ok"></i>', 'offText' => '<i class="glyphicon glyphicon-remove"></i>', 'onColor' => 'success', 'offColor' => 'danger',],])->label('Activo') ?>
-</div>
-<?= Html::submitButton('Modificar', ['class' => 'btn btn-primary']) ?>
-
 <?php $form->end() ?>
