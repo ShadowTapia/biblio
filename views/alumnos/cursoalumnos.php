1ab87480d4c36c43996c2eb5fa440332a6c09772
Marcelo Tapia <chelo.tapia@gmail.com>
2023-07-18 12:52:26 -0400

    actualización vista curso alumnos

--- views/alumnos/cursoalumnos.php
+++ views/alumnos/cursoalumnos.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: chelo
@@ -11,13 +12,13 @@ use kartik\spinner\Spinner;
 use yii\helpers\Html;
 use yii\widgets\Pjax;
 
-$this->title='Alumnos por Curso '. Yii::$app->session->get('nameAno');
+$this->title = 'Alumnos por Curso ' . Yii::$app->session->get('nameAno');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div id="variable" style="display: none;"><?= $count ?></div>
 <?php
-    $this->registerJs(
-            '$("document").ready(function(){                    
+$this->registerJs(
+    '$("document").ready(function(){                    
                     var x = document.getElementById("toggleSearch");
                     var myParam = document.getElementById("variable").innerHTML;
                     if(myParam>0){
@@ -28,13 +29,13 @@ $this->params['breadcrumbs'][] = $this->title;
                          x.style.display="none";
                     }
             });'
-    );
+);
 ?>
 
 <div class="alumnos-index">
     <h1><?= Html::encode($this->title) ?></h1>
     <h6>*No incluye alumnos retirados</h6>
-    <?= $this->render('_form',['model'=>$model,]) ?>
+    <?= $this->render('_form', ['model' => $model,]) ?>
 
     <div id="well" style="display: none">
         <?= Spinner::widget([
@@ -46,81 +47,80 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
     <div id="toggleSearch" style="display: none">
         <p id="divImprime" class="row" style="text-align: right">
-            <?= Html::a('<span class="glyphicon glyphicon-print"></span> Imprimir', ['reporte'],['class' => 'btn btn-warning','target'=>'_blank','data-toggle'=>'tooltip','title'=>'Generara un reporte en formato pdf']) ?>
+            <?= Html::a('<span class="glyphicon glyphicon-print"></span> Imprimir', ['reporte'], ['class' => 'btn btn-warning', 'target' => '_blank', 'data-toggle' => 'tooltip', 'title' => 'Generara un reporte en formato pdf']) ?>
         </p>
         <?php Pjax::begin([
-           'id'=>'alumnos',
-            'timeout'=>false,
-            'enablePushState'=>false,
-            'clientOptions'=>['method'=>'GET']
+            'id' => 'alumnos',
+            'timeout' => false,
+            'clientOptions' => ['method' => 'POST']
         ]); ?>
 
         <?php
-            $gridColumns = [
-                [
-                    'class' => 'yii\grid\SerialColumn',
-                    'headerOptions' => ['width' => '20px'],
-                    'contentOptions' => ['style' => 'width: 20px;', 'class' => 'text-center'],
-                ],
-                //Run alumno
-                [
-                    'attribute' => 'rutalumno',
-                    'label' => 'Run',
-                    'headerOptions' => ['width' => '120px'],
-                    'value' => function($model){
-                        return Yii::$app->formatter->asDecimal($model->rutalumno,0). "-" .  $model->digrut;
-                    }
-                ],
-                //Apellido paterno
-                [
-                    'attribute' => 'paternoalu',
-                    'label' => 'A. Paterno',
-                    'format' => 'html',
-                    'value' => function($model){
-                        return !empty($model->paternoalu) ? $model->paternoalu : '<span class="glyphicon glyphicon-question-sign"></span>';
-                    }
-                ],
-                //Apellido materno
-                [
-                    'attribute' => 'maternoalu',
-                    'label' => 'A. Materno',
-                    'format' => 'html',
-                    'value' => function($model){
-                        return !empty($model->maternoalu) ? $model->maternoalu : '<span class="glyphicon glyphicon-question-sign"></span>';
-                    }
-                ],
-                //Nombre alumno
-                [
-                    'attribute' => 'nombrealu',
-                    'label' => 'Nombres',
-                    'format' => 'html',
-                    'value' => function($model){
-                        return !empty($model->nombrealu) ? $model->nombrealu : '<span class="glyphicon glyphicon-question-sign"></span>';
-                    }
-                ]
-            ];
+        $gridColumns = [
+            [
+                'class' => 'yii\grid\SerialColumn',
+                'headerOptions' => ['width' => '20px'],
+                'contentOptions' => ['style' => 'width: 20px;', 'class' => 'text-center'],
+            ],
+            //Run alumno
+            [
+                'attribute' => 'rutalumno',
+                'label' => 'Run',
+                'headerOptions' => ['width' => '120px'],
+                'value' => function ($model) {
+                    return Yii::$app->formatter->asDecimal($model->rutalumno, 0) . "-" .  $model->digrut;
+                }
+            ],
+            //Apellido paterno
+            [
+                'attribute' => 'paternoalu',
+                'label' => 'A. Paterno',
+                'format' => 'html',
+                'value' => function ($model) {
+                    return !empty($model->paternoalu) ? $model->paternoalu : '<span class="glyphicon glyphicon-question-sign"></span>';
+                }
+            ],
+            //Apellido materno
+            [
+                'attribute' => 'maternoalu',
+                'label' => 'A. Materno',
+                'format' => 'html',
+                'value' => function ($model) {
+                    return !empty($model->maternoalu) ? $model->maternoalu : '<span class="glyphicon glyphicon-question-sign"></span>';
+                }
+            ],
+            //Nombre alumno
+            [
+                'attribute' => 'nombrealu',
+                'label' => 'Nombres',
+                'format' => 'html',
+                'value' => function ($model) {
+                    return !empty($model->nombrealu) ? $model->nombrealu : '<span class="glyphicon glyphicon-question-sign"></span>';
+                }
+            ]
+        ];
         ?>
 
         <?= GridView::widget([
-                'dataProvider'=>$dataProvider,
-                'columns'=>$gridColumns,
-                'export'=>false,
-                'pjax'=>true,
-                'emptyText'=>'No hay resultados para este Curso',
-                'showPageSummary' => false,
-                'panel'=>[
-                    'type' => 'primary',
-                    'heading' => '<h3 class="panel-title">Listado Curso ' . ' ' . $nomcurso . ' ' . Yii::$app->session->get('nameAno') . '</h3>',
-                    'footer' => false
+            'dataProvider' => $dataProvider,
+            'columns' => $gridColumns,
+            'export' => false,
+            'pjax' => true,
+            'pjaxSettings' => [
+                'options' => [
+                    'enablePushState' => false,
+                    'enableReplaceState' => true,
                 ]
+            ],
+            'emptyText' => 'No hay resultados para este Curso',
+            'showPageSummary' => false,
+            'panel' => [
+                'type' => 'primary',
+                'heading' => '<h3 class="panel-title">Listado Curso ' . ' ' . $nomcurso . ' ' . Yii::$app->session->get('nameAno') . '</h3>',
+                'footer' => false
+            ]
         ]); ?>
         <?php Pjax::end(); ?>
     </div>
 
 </div>
-
-
-
-
-
-
